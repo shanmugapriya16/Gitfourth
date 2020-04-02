d22c3f87b01246471e4d15183ce0804b3d19c5a6
shanmugapriya16 <mshanmugapriya16@gmail.com>
2020-04-02 23:30:38 +0530

    Fourth program

---
+++ src/org/excep/Excep.java
@@ -0,0 +1,32 @@
+package org.excep;
+
+public class Excep {
+public static void main(String[] args) {
+	try
+	{
+		System.out.println("1");
+		int sum = 9/0;
+		
+		System.out.println("2");
+		
+		
+		
+	}
+	catch(ArithmeticException e) {
+		System.out.println("3");
+	}
+
+	
+catch(Exception e) {
+	System.out.println("4");
+}
+	
+	finally {
+		System.out.println("5");
+	}
+	
+	
+}
+	
+	
+}
